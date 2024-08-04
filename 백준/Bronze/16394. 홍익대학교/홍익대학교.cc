cb50bc89ef57b7763cbc5db087fd885df02a432e
강수용 <57248423+Kang-suyong@users.noreply.github.com>
2024-08-04 23:47:04 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <list>
+#include <typeinfo>
+#define loop(i,n) for(int i = 0; i< n; i++)
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    cout << n - 1946;
+    
+}
